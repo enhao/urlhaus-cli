741b7c2ce8dced74ea38517f09faf820dbe4b296
Enhao <enhao.mobile@gmail.com>
2019-02-22 10:55:59 +0800

    feat(cmd/payload): modify output format

--- cmd/payload.go
+++ cmd/payload.go
@@ -21,15 +21,44 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"strings"
+	"text/template"
 
 	"github.com/spf13/cobra"
 )
+
+const payloadTempl = `{{if eq .query_status "ok"}}Malware Payload Infomation:
+  Type: {{.file_type}}
+  Size: {{.file_size}}
+  Hash:
+    - MD5:    {{.md5_hash}}
+    - SHA256: {{.sha256_hash}}
+
+  {{if .signature}}Signature:  {{.signature}}{{end}}
+  First seen: {{.firstseen}}
+  {{if .lastseen}}Last seen:  {{.lastseen}}{{end}}
+  Number of URLs observation: {{.url_count}}
+  URLhaus reference: {{.urlhaus_reference}}
+  Sample download: {{.urlhaus_download}}
+  {{if .virustotal}}VirusTotal: {{.virustotal.percent}}% ({{.virustotal.result}})
+    Link:     {{.virustotal.link}}
+  {{end}}
+  List of malware URLs associated with this payload (max 100):{{range .urls}}
+    * {{.url}}
+      Status:       {{.url_status}}
+      URLhaus:
+        Reference:  {{.urlhaus_reference}}
+        First seen: {{.firstseen}}{{if .lastseen}}
+        Last seen:  {{.lastseen}}{{end}}
+{{end}}{{else}}{{.query_status}}{{end}}
+`
 
 var hashType string
 
@@ -52,12 +81,32 @@ URLhaus has retrieved.`,
 		if err != nil {
 			log.Fatal(err)
 		}
-		content, err := ioutil.ReadAll(resp.Body)
-		resp.Body.Close()
+		defer resp.Body.Close()
+
+		b, err := ioutil.ReadAll(resp.Body)
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Printf("%s", content)
+
+		if len(b) == 0 {
+			return
+		}
+
+		if rawOutput {
+			fmt.Printf("%s", b)
+			return
+		}
+
+		t := template.Must(template.New("").Parse(payloadTempl))
+
+		m := map[string]interface{}{}
+		if err := json.Unmarshal([]byte(b), &m); err != nil {
+			log.Fatal(err)
+		}
+
+		if err := t.Execute(os.Stdout, m); err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
